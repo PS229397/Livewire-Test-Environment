a173f39599e1bdae7301a8b27365920bf6d86ea4
PS229397 <PS229397@summacollege.nl>
2025-11-25 12:16:11 +0100

    played around with some extra functions

--- 1. Counter/app/Livewire/Counter.php
+++ 1. Counter/app/Livewire/Counter.php
@@ -4,6 +4,8 @@ namespace App\Livewire;
 
 use Livewire\Component;
 
+use function Laravel\Prompts\alert;
+
 class Counter extends Component
 {
     public function render()
@@ -12,12 +14,29 @@ class Counter extends Component
     }
 
     public $count = 0;
+    public $msg = "";
+
     public function increment()
     {
-        $this->count++;
+        if ($this->count != 10) {
+            $this->count++;
+            $this->msg = "";
+        } else {
+            $this->msg = "Count cant be higher than 10.";
+        }
     }
     public function decrement()
     {
-        $this->count--;
+        if ($this->count != -5) {
+            $this->count--;
+            $this->msg = "";
+        } else {
+            $this->msg = "Count cant be lower than -5.";
+        }
+    }
+    public function resetCount()
+    {
+        $this->count = 0;
+        $this->msg = "";
     }
 }
--- 1. Counter/resources/views/livewire/counter.blade.php
+++ 1. Counter/resources/views/livewire/counter.blade.php
@@ -3,5 +3,9 @@
         Counter: {{ $count }}
     </div>
     <button wire:click="decrement">-</button>
+    <button wire:click="resetCount">Reset</button>
     <button wire:click="increment">+</button>
+    <div>
+        {{ $msg }}
+    </div>
 </div>
