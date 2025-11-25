7ebf22b4340b3d0492609caee27f8541c522a7f6
PS229397 <PS229397@summacollege.nl>
2025-11-25 13:14:56 +0100

    expanded the counter some more

--- 1. Counter/app/Livewire/Counter.php
+++ 1. Counter/app/Livewire/Counter.php
@@ -13,27 +13,41 @@ class Counter extends Component
         return view('livewire.counter');
     }
 
+    //public variable storage
     public $count = 0;
     public $msg = "";
 
+    //function to increment until $count hits 10
     public function increment()
     {
+        //if $count is not 10 keep adding to counter, removes $msg if needed
         if ($this->count != 10) {
             $this->count++;
             $this->msg = "";
-        } else {
+        }
+
+        //if $count is exactly 10 let the user know it cant go higher in $msg
+        if ($this->count == 10) {
             $this->msg = "Count cant be higher than 10.";
         }
     }
+
+    //function to decrement until $count hits -5
     public function decrement()
     {
+        //if $count is not -5 keep subtracting from counter, removes $msg if needed
         if ($this->count != -5) {
             $this->count--;
             $this->msg = "";
-        } else {
+        }
+
+        //if $count is exactly -5 let the user know it cant go lower in $msg
+        if ($this->count == -5) {
             $this->msg = "Count cant be lower than -5.";
         }
     }
+
+    //function to reset $count to 0 also resets $msg
     public function resetCount()
     {
         $this->count = 0;
--- 1. Counter/resources/views/livewire/counter.blade.php
+++ 1. Counter/resources/views/livewire/counter.blade.php
@@ -2,9 +2,9 @@
     <div>
         Counter: {{ $count }}
     </div>
-    <button wire:click="decrement">-</button>
+    <button wire:click="decrement" @disabled($count == -5)>-</button>
     <button wire:click="resetCount">Reset</button>
-    <button wire:click="increment">+</button>
+    <button wire:click="increment" @disabled($count == 10)>+</button>
     <div>
         {{ $msg }}
     </div>
