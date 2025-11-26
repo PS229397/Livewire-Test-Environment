6239d78548e7012de8973300d98cbf9fbf857abf
PS229397 <PS229397@summacollege.nl>
2025-11-26 14:27:16 +0100

    basic todo list done

--- 2. Todo/app/Livewire/TodoList.php
+++ 2. Todo/app/Livewire/TodoList.php
@@ -6,8 +6,28 @@ use Livewire\Component;
 
 class TodoList extends Component
 {
+    //global variable list
+    public $newTodo = '';
+    public $todos = [];
+
     public function render()
     {
         return view('livewire.todo-list');
+    }
+
+    //function to add an item to the todo list
+    public function addTodo(){
+        //adds text from the newTodo input into the todos array
+        $this->todos[] = $this->newTodo;
+        //clears the newTodo input for the next item
+        $this->newTodo = '';
+    }
+
+    //function to remove specified item out of the todo list $index gets passed down from button
+    public function removeTodo($index){
+        //removes the specified item at $index from the todos array
+        unset($this->todos[$index]);
+        //makes a duplicate of the array with all remaining items nicely indexed, pushes it to the todos array
+        $this->todos = array_values($this->todos);
     }
 }
--- 2. Todo/resources/views/livewire/todo-list.blade.php
+++ 2. Todo/resources/views/livewire/todo-list.blade.php
@@ -1,3 +1,15 @@
 <div>
     {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+    <h1 class="text-2xl font-bold mb-4">New item</h1>
+    <input wire:model.live="newTodo" type="text" placeholder="new item">
+    <button wire:click="addTodo" @disabled(empty($newTodo)) class="ml-2 px-4 py-2 bg-blue-500 text-white rounded">Add</button>
+    <br><br>
+
+    <h2 class="text-xl font-semibold mt-6 mb-2">Todo List:</h2>
+    @foreach($todos as $index => $todo)
+    <li class="mb-2">
+        {{ $todo }}
+        <button wire:click="removeTodo({{ $loop->index }})" class="ml-2 px-2 py-1 bg-red-500 text-white rounded">Remove</button>
+    </li>
+    @endforeach
 </div>
